7ef16b782d031caf6e206b22c074fd82056b71a0
Alexei Karamzin <1999aleks_1999@mail.ru>
2022-06-25 12:42:38 +0300

    formik and login pages

--- src/main/m1-ui/header/pages/login/Login.tsx
+++ src/main/m1-ui/header/pages/login/Login.tsx
@@ -1,13 +1,78 @@
-import {Container,Grid} from '@mui/material';
+import {Button, Checkbox, Container, FormControl, FormControlLabel, FormGroup, FormLabel, Grid, TextField} from '@mui/material';
 import React from 'react';
 import style from './Login.module.css'
+import {useFormik} from "formik";
 
 function Login() {
+
+    const formik = useFormik({
+        validate: (value) => {
+            if (!value.password) {
+                return {
+                    password: 'Password is required'
+                }
+            }
+            if (!value.email) {
+                return {
+                    email: 'Email is required'
+                }
+            }
+        },
+        initialValues: {
+            email: '',
+            password: '',
+            rememberMe: false
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values));
+        },
+    });
+
     return (
         <div className={style.login}>
             <Container fixed>
-                <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-
+                <Grid container justifyContent={'center'}>
+                    <form onSubmit={formik.handleSubmit}>
+                        <FormControl>
+                            <FormLabel>
+                                <FormLabel>
+                                    <p>To log in get registered
+                                        <a href={'https://social-network.samuraijs.com/'}
+                                           target={'_blank'}> here
+                                        </a>
+                                    </p>
+                                    <p>or use common test account credentials:</p>
+                                    <p>Email: free@samuraijs.com</p>
+                                    <p>Password: free</p>
+                                </FormLabel>
+                                <FormGroup>
+                                    <TextField
+                                        label="Email"
+                                        margin="normal"
+                                        {...formik.getFieldProps('email')}
+                                    />
+                                    {formik.errors.email ? <div>{formik.errors.email}</div> : null}
+                                    <TextField
+                                        type="password"
+                                        label="Password"
+                                        margin="normal"
+                                        {...formik.getFieldProps('password')}
+                                    />
+                                    {formik.errors.password ? <div>{formik.errors.password}</div> : null}
+                                    <FormControlLabel
+                                        label={'Remember me'}
+                                        control={<Checkbox
+                                            checked={formik.values.rememberMe}
+                                            {...formik.getFieldProps('rememberMe')}
+                                        />}
+                                    />
+                                    <Button type={'submit'} variant={'contained'} color={'primary'}>
+                                        Login
+                                    </Button>
+                                </FormGroup>
+                            </FormLabel>
+                        </FormControl>
+                    </form>
                 </Grid>
                 {/*<Routes>
                     <Route path="/" element={<TodolistsList demo={demo}/>} />
