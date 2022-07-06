ee0cf43380f0a2faba817a5284e5579be96323db
Maxim Predko <maxpredko@gmail.com>
2022-07-06 19:39:25 +0300

    change header component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,22 +1,45 @@
-import React from 'react';
-import {NavLink} from 'react-router-dom';
-import {PATH} from '../common/routes/RoutesConstants';
-import style from './Header.module.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { PATH } from "../common/routes/RoutesConstants";
+import cardsIcon from "../../assets/img/cardsIcon.png";
+import profileIcon from "../../assets/img/profileIcon.png";
+import style from "./Header.module.css";
 
-const Header: React.FC = () => {
-    return (
-        <div>
-            <nav className={style.navlink_list}>
-                <NavLink to={PATH.LOGIN}>Login</NavLink>
-                <NavLink to={PATH.REGISTRATION}>Registration</NavLink>
-                <NavLink to={PATH.PROFILE}>Profile</NavLink>
-                <NavLink to={PATH.ERROR404}>Error_404</NavLink>
-                <NavLink to={PATH.PASSWORD_RECOVERING}>Password_recovering</NavLink>
-                <NavLink to={PATH.NEW_PASSWORD}>New_password</NavLink>
-                <NavLink to={PATH.TEST_PAGE}>Test_page</NavLink>
-            </nav>
-        </div>
-    );
+export const Header: React.FC = () => {
+  return (
+    <header className={style.header}>
+      <nav className={style.nav_list}>
+        {/* //изменить на packs */}
+
+        <NavLink
+          to={PATH.CARDS_LIST}
+          className={({ isActive }) =>
+            style.link + " " + (isActive ? style.active : "")
+          }
+        >
+          {/* <div
+            style={{
+              backgroundColor: `url(${cardsIcon})}`,
+            }}
+          /> */}
+          <img src={cardsIcon} className={style.icon} alt="card-icon" />
+          Packs list
+        </NavLink>
+
+        <NavLink
+          to={PATH.PROFILE}
+          className={({ isActive }) =>
+            style.link + " " + (isActive ? style.active : "")
+          }
+        >
+          <img src={profileIcon} className={style.icon} alt="card-icon" />
+          Profile
+        </NavLink>
+
+        {/*    <NavLink to={PATH.LOGIN}>Login</NavLink> 
+        <NavLink to={PATH.PASSWORD_RECOVERING}>Password_recovering</NavLink>
+        <NavLink to={PATH.NEW_PASSWORD}>New_password</NavLink> */}
+      </nav>
+    </header>
+  );
 };
-
-export default Header;
